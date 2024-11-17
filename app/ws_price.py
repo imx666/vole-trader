d7398d33f6f5efd2a1fe1a2175b5877c8ecf0d80
imx666 <3490980474@qq.com>
2024-11-17 16:36:23 +0800

    增加断开重连

--- app/ws_price.py
+++ app/ws_price.py
@@ -1,4 +1,7 @@
 import asyncio
+from datetime import datetime
+import socket
+
 import websockets
 import json
 
@@ -23,7 +26,6 @@ target_stock = "FLOKI-USDT"
 
 async def main():
 
-
     # 订阅产品频道的消息
     subscribe_msg = {
         "op": "subscribe",
@@ -35,32 +37,75 @@ async def main():
             }
         ]
     }
+    while True:
+        reconnect_attempts = 0
+        try:
+            async with websockets.connect('wss://ws.okx.com:8443/ws/v5/public') as websocket:
+                # 发送订阅请求
+                await websocket.send(json.dumps(subscribe_msg))
+                subscribe_response = await websocket.recv()
+                print("订阅响应: ", subscribe_response)
 
-    async with websockets.connect('wss://ws.okx.com:8443/ws/v5/public') as websocket:
-        # 发送订阅请求
-        await websocket.send(json.dumps(subscribe_msg))
-        subscribe_response = await websocket.recv()
-        print("订阅响应: ", subscribe_response)
+                # 持续监听增量数据
+                while True:
+                    try:
+                        # response = await websocket.recv()
+                        response = await asyncio.wait_for(websocket.recv(), timeout=25)
 
-        # 持续监听增量数据
-        while True:
-            try:
-                response = await websocket.recv()
-                # print("收到增量数据: ", response)
-                # 将字符串转换为字典
-                data_dict = json.loads(response)
-                # 打印转换后的字典
-                os.system("clear")
-                price = data_dict["data"][0]["px"]
-                num = data_dict["data"][0]["sz"]
+                        # print("收到增量数据: ", response)
+                        # 将字符串转换为字典
+                        data_dict = json.loads(response)
+                        # os.system("clear")
+                        price = data_dict["data"][0]["px"]
+                        num = data_dict["data"][0]["sz"]
 
-                print(f"标的:{target_stock}")
-                print(f"成交价格:{price}")
-                print(f"成交数量:{num}")
+                        print(f"标的:{target_stock}")
+                        print(f"成交价格:{price}")
+                        print(f"成交数量:{num}")
 
-                # 如果需要处理消息，可以在这里添加逻辑
-            except websockets.exceptions.ConnectionClosedOK:
-                print("over")
+                    except (asyncio.TimeoutError, websockets.exceptions.ConnectionClosed) as e:
+                        try:
+                            await websocket.send('ping')
+                            res = await websocket.recv()
+                            print(res)
+                            continue
+
+                        except Exception as e:
+                            print("连接关闭，正在重连……")
+                            break
+
+
+        except websockets.exceptions.InvalidURI as e:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{current_time} 连接断开，正在重连……")
+            print('uri异常！')
+            await asyncio.sleep(5)  # 等待5秒后重试
+
+        # 重新尝试连接，使用指数退避策略
+        except websockets.exceptions.ConnectionClosed as e:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{current_time} 连接断开，正在重连……")
+            print(f"Connection closed: {e}")
+            reconnect_attempts += 1
+            wait_time = min(2 ** reconnect_attempts, 60)  # 最大等待时间为60秒
+            print(f"Reconnecting in {wait_time} seconds...")
+            await asyncio.sleep(wait_time)
+
+        # 重新尝试连接，使用指数退避策略,针对于“远程计算机拒绝网络连接”错误
+        except socket.error as e:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{current_time} 连接断开，正在重连……")
+            print(f"Connection closed: {e}")
+            reconnect_attempts += 1
+            wait_time = min(2 ** reconnect_attempts, 60)  # 最大等待时间为60秒
+            print(f"Reconnecting in {wait_time} seconds...")
+            await asyncio.sleep(wait_time)
+
+        except Exception as e:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{current_time} 连接断开，正在重连……")
+            print('其他', e)
+
 
 
 if __name__ == '__main__':
