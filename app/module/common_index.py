6be7d8f06f6cd76d266080e698c78b4efbcba766
imx666 <3490980474@qq.com>
2024-11-18 15:07:49 +0800

    动态控制周期

--- app/module/common_index.py
+++ app/module/common_index.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 
-def get_DochianChannel(total_candle):
+# PERIOD = 5
+
+def get_DochianChannel(total_candle, PERIOD):
     # 获取唐奇安窗口
     history_max_price = 0
     history_min_price = 9999999999
 
-    part_candle = total_candle[:20]
+    # part_candle = total_candle[:20]
+    part_candle = total_candle[:PERIOD]
 
     # print(part_candle)
     # print(total_candle)
@@ -24,7 +27,7 @@ def get_DochianChannel(total_candle):
     return history_max_price, history_min_price
 
 
-def get_ATR(total_candle):
+def get_ATR(total_candle, PERIOD):
     part_candle = total_candle[:20]
     columns = ['timestamp', 'open', 'high', 'low', 'close']
     df = pd.DataFrame(part_candle, columns=columns)
@@ -38,8 +41,9 @@ def get_ATR(total_candle):
                         abs(row['low'] - row['previous_close'])), axis=1)
 
     # 计算 ATR（20天简单移动平均）
-    n = 20
-    df['atr'] = df['tr'].rolling(window=n).mean()
+    # n = 20
+    # df['atr'] = df['tr'].rolling(window=n).mean()
+    df['atr'] = df['tr'].rolling(window=PERIOD).mean()
 
     # 设置显示所有行
     pd.set_option('display.max_rows', None)
@@ -51,6 +55,7 @@ def get_ATR(total_candle):
 
     # 输出该值
     last_value = float(last_value)
+    last_value = round(last_value, 10)
     # print(last_value)
 
     return last_value
