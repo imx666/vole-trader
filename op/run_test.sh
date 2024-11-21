921ebadfd56b6c63d4d55be5d74b1624c7a1db81
imx666 <3490980474@qq.com>
2024-11-21 20:05:31 +0800

    批量画图

--- op/run_test.sh
+++ op/run_test.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 
-#sh ./op/env.sh   #sh指令会创建一个新的子进程，从而使得父进程无法进入虚拟环境
-#"""source的含义在当前 shell 进程中执行指定的脚本文件，而不是启动一个新的子进程。"""
-#"""source命令可能无法找到，所以可以使用.这个符号来代替"""
-. ./op/env.sh
+cd ../app
+export target_stock=BTC-USDT
+python simulate_combat.py &
 
+export target_stock=LUNC-USDT
+python simulate_combat.py &
 
+export target_stock=FLOKI-USDT
+python simulate_combat.py &
 
-#导入环境变量
-if [ ! -f .env.dev ]; then
-  echo ".env.dev 文件不存在，请创建并配置必要的环境变量。"
-  exit 1
-fi
-export $(xargs <.env.dev)
-export APP_ENV="test"
+export target_stock=OMI-USDT
+python simulate_combat.py &
 
+export target_stock=DOGE-USDT
+python simulate_combat.py &
 
-# 启动 supervisord
-supervisord -n -c ./op/supervisord.conf
-
-echo "supervisord start success!"
+export target_stock=PEPE-USDT
+python simulate_combat.py &
 
 
 
