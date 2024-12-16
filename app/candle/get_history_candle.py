eb821500758c38d9dc40be15656aa9754d4fcbd0
imx666 <3490980474@qq.com>
2024-12-17 02:33:57 +0800

    修复BASE_DIR，新增自选candle周期

--- app/candle/get_history_candle.py
+++ app/candle/get_history_candle.py
@@ -19,24 +19,29 @@ if __name__ == '__main__':
     project_path = Path(__file__).resolve().parent  # 此脚本的运行"绝对"路径
     dotenv_path = os.path.join(project_path, '../.env.dev')  # 指定.env.dev文件的路径
     load_dotenv(dotenv_path)  # 载入环境变量
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
     from module.genius_trading import GeniusTrader
     from utils.files import find_or_create_doc
 
-    target_stock = "LUNC-USDT"
-    target_stock = "BTC-USDT"
-    target_stock = "FLOKI-USDT"
-    target_stock = "OMI-USDT"
-    target_stock = "DOGE-USDT"
-    target_stock = "PEPE-USDT"
+    # target_stock = "LUNC-USDT"
+    # target_stock = "BTC-USDT"
+    target_stock = "ETH-USDT"
+    # target_stock = "FLOKI-USDT"
+    # target_stock = "OMI-USDT"
+    # target_stock = "DOGE-USDT"
+    # target_stock = "PEPE-USDT"
+    # target_stock = "RACA-USDT"
 
     pre_day = None
-    genius_trader = GeniusTrader()
+    # pre_day = 1715587200000
 
+    genius_trader = GeniusTrader()
     long_period_candle = []
-    for _ in range(4):
-        total_candle = genius_trader.stock_candle(target_stock, after=pre_day)
+    for _ in range(23):
+        # period = "1H"
+        period = "4H"
+        total_candle = genius_trader.stock_candle(target_stock, after=pre_day, period=period)
         # total_candle.reverse()  # 由于时间，倒序
         pre_day = total_candle[-1][0]
         print(pre_day)
@@ -50,6 +55,7 @@ if __name__ == '__main__':
     # print(len(long_period_candle))
 
     total_path = os.path.join(BASE_DIR, f"./data/{target_stock}.json")
+    # total_path = os.path.join(BASE_DIR, f"./data/{target_stock}-longtest.json")
     find_or_create_doc(total_path, 'json')
     json_data = json.dumps(long_period_candle)
     with open(total_path, 'w', encoding='utf-8') as f:
