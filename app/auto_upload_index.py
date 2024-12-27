a7ce89f2a656854baa12ccdd02b2601ad3f3fd5e
imx666 <3490980474@qq.com>
2024-12-28 01:09:51 +0800

    新增try，防止意外退出

--- app/auto_upload_index.py
+++ app/auto_upload_index.py
@@ -30,27 +30,38 @@ def update_job():
         "DOGE-USDT",
         "PEPE-USDT",
         "ETH-USDT",
+        "XRP-USDT",
     ]
 
     for target_stock in target_stock_li:
-        total_candle = genius_trader.stock_candle(target_stock, period="4H")
-        # print(total_candle)
+        # 尝试爬取
+        for i in range(1, 5):
+            if i == 4:
+                LOGGING.error(f"--彻底失败\n")
+                continue
+            try:
+                total_candle = genius_trader.stock_candle(target_stock, period="4H")
+                # print(total_candle)
+                PERIOD = 3
+                history_max_price, history_min_price = get_DochianChannel(total_candle, PERIOD)
+                ATR = get_ATR(total_candle, PERIOD)
 
-        PERIOD = 3
-        history_max_price, history_min_price = get_DochianChannel(total_candle, PERIOD)
-        ATR = get_ATR(total_candle, PERIOD)
+                redis_okx = redis.Redis.from_url(redis_url)
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                redis_okx.hset(f"common_index:{target_stock}", 'update_time', current_time)
+                redis_okx.hset(f"common_index:{target_stock}",
+                               mapping={'history_max_price': history_max_price, 'history_min_price': history_min_price,
+                                        'ATR': ATR})
 
-        redis_okx = redis.Redis.from_url(redis_url)
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        redis_okx.hset(f"common_index:{target_stock}", 'update_time', current_time)
-        redis_okx.hset(f"common_index:{target_stock}",
-                       mapping={'history_max_price': history_max_price, 'history_min_price': history_min_price,
-                                'ATR': ATR})
+                LOGGING.info(f"target_stock:{target_stock}")
+                LOGGING.info(f"history_max_price:{history_max_price}")
+                LOGGING.info(f"history_min_price:{history_min_price}")
+                LOGGING.info(f"ATR:{ATR}\n")
+                break
+            except Exception as e:
+                LOGGING.warning(f"爬取失败--第{i}次:{e}")
+                time.sleep(5)
 
-        LOGGING.info(f"target_stock:{target_stock}")
-        LOGGING.info(f"history_max_price:{history_max_price}")
-        LOGGING.info(f"history_min_price:{history_min_price}")
-        LOGGING.info(f"ATR:{ATR}\n")
 
 
 
