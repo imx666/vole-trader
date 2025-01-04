3a14e0b7840af42f17e1331ef17fc6da6b764c16
imx666 <3490980474@qq.com>
2025-01-05 03:49:04 +0800

    对重置redis中hold_info的逻辑进行升级

--- app/init_hold_info.py
+++ app/init_hold_info.py
@@ -32,55 +32,58 @@ if __name__ == '__main__':
     target_stock = "BTC-USDT"
     target_stock = "DOGE-USDT"
     target_stock = "ETH-USDT"
-    # target_stock = "FLOKI-USDT"
+    target_stock = "FLOKI-USDT"
+    target_stock = "XRP-USDT"
     # target_stock = "OMI-USDT"
     # target_stock = "PEPE-USDT"
-    target_stock = "XRP-USDT"
     # target_stock = "LUNC-USDT"
 
 
-    # 初始化
-    redis_okx.hset(f"hold_info:{target_stock}",
-                   mapping={
-                       'execution_cycle': "ready",
-                       'pending_order': 0,
-                       'tradeFlag': 'all-auth',
-                       'long_position': 0,
-                       'sell_times': 0,
-                       '<init_balance>': 100,
-                       '<risk_rate>': 0.0035,
-                       '<max_long_position>': 3,
-                       '<max_sell_times>': 3,
-                   })
-
-    hold_info = HoldInfo(target_stock)
-
-
-
-    # # 重启redis时
-    # from module.trade_records import TradeRecordManager
-    # # execution_cycle = sqlManager.last_execution_cycle(strategy_name)  # 获取编号
-    # # execution_cycle = "自己看数据库是哪个编号"
-    # execution_cycle = "TURTLE-FLOKI-20241221_0001"
-    # sqlManager = TradeRecordManager(target_stock, strategy_name="TURTLE")
-    # # execution_cycle = "ready"
-    # long_position = sqlManager.get(execution_cycle, "long_position")
-    # sell_times = sqlManager.get(execution_cycle, "sell_times")
-    # build_price = sqlManager.get(execution_cycle, "build_price"),
-    # balance_delta = sqlManager.get(execution_cycle, "balance_delta")  # 虽然需要传编号，但是计算价差是用不着的
-    # init_balance = 100 + balance_delta
-    # print(init_balance)
-    # print(balance_delta)
-    #
+    # # 初始化
     # redis_okx.hset(f"hold_info:{target_stock}",
     #                mapping={
-    #                    'execution_cycle': execution_cycle,
-    #                    'pending_order': 0,  # pending_order是防止重复挂建仓订单的
+    #                    'execution_cycle': "ready",
+    #                    'pending_order': 0,
     #                    'tradeFlag': 'all-auth',
-    #                    'long_position': long_position,
-    #                    'sell_times': sell_times,
-    #                    '<init_balance>': init_balance,
+    #                    'long_position': 0,
+    #                    'sell_times': 0,
+    #                    '<init_balance>': 100,
     #                    '<risk_rate>': 0.0035,
     #                    '<max_long_position>': 3,
     #                    '<max_sell_times>': 3,
     #                })
+    #
+    # hold_info = HoldInfo(target_stock)
+
+
+
+    # 重启redis时
+    from module.trade_records import TradeRecordManager
+    sqlManager = TradeRecordManager(target_stock, strategy_name="TURTLE")
+    # execution_cycle = "TURTLE-DOGE-20250104_0002"  # 自己看数据库是哪个编号
+    execution_cycle = sqlManager.last_execution_cycle()  # 或者是自动选择到最后一条
+    print(execution_cycle)
+    trade_record = sqlManager.get_trade_record(execution_cycle)
+    if trade_record["operation"] == 'close' and trade_record["state"] == 'filled':
+        execution_cycle = "ready"
+
+
+    long_position = sqlManager.get(execution_cycle, "long_position")
+    sell_times = sqlManager.get(execution_cycle, "sell_times")
+    balance_delta = sqlManager.get(execution_cycle, "balance_delta")  # 虽然需要传编号，但是计算价差是用不着的
+    init_balance = 50 + balance_delta
+    print(init_balance)
+    print(balance_delta)
+
+    redis_okx.hset(f"hold_info:{target_stock}",
+                   mapping={
+                       'execution_cycle': execution_cycle,
+                       'pending_order': 0,  # pending_order是防止重复挂建仓订单的
+                       'tradeFlag': 'all-auth',
+                       'long_position': long_position,
+                       'sell_times': sell_times,
+                       '<init_balance>': init_balance,
+                       '<risk_rate>': 0.0035,
+                       '<max_long_position>': 3,
+                       '<max_sell_times>': 3,
+                   })
