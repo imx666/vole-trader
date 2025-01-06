0e0c22c5a8db9ca5f12ff96000b79ee577912193
wilson_microsoft <3354949827@qq.com>
2025-01-06 23:36:12 +0800

    适应终端载入环境

--- app/candle/get_history_candle.py
+++ app/candle/get_history_candle.py
@@ -4,23 +4,30 @@ import logging.config
 import time
 from datetime import datetime
 
-from utils.logging_config import Logging_dict
 
-logging.config.dictConfig(Logging_dict)
-LOGGING = logging.getLogger("app_01")
+import sys
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# 锁定系统运行路径
+project_path = Path(__file__).resolve().parent  # 此脚本的运行"绝对"路径
+dotenv_path = os.path.join(project_path, '../')
+sys.path.append(dotenv_path)
+
+dotenv_path = os.path.join(project_path, '../../.env.dev')  # 指定.env.dev文件的路径
+load_dotenv(dotenv_path)  # 载入环境变量
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# from utils.logging_config import Logging_dict
+
+# logging.config.dictConfig(Logging_dict)
+# LOGGING = logging.getLogger("app_01")
 
 
 
 if __name__ == '__main__':
 
-    import os
-    from pathlib import Path
-    from dotenv import load_dotenv
 
-    project_path = Path(__file__).resolve().parent  # 此脚本的运行"绝对"路径
-    dotenv_path = os.path.join(project_path, '../.env.dev')  # 指定.env.dev文件的路径
-    load_dotenv(dotenv_path)  # 载入环境变量
-    BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
     from module.super_okx import GeniusTrader
     from utils.files import find_or_create_doc
@@ -34,17 +41,16 @@ if __name__ == '__main__':
     # target_stock = "PEPE-USDT"
     # target_stock = "RACA-USDT"
 
-    target_stock = "JST-USDT"
-    target_stock = "ZRX-USDT"
-    target_stock = "ZIL-USDT"
-    target_stock = "BOME-USDT"
-    target_stock = "ASTR-USDT"
-    target_stock = "GOAT-USDT"
-    target_stock = "MORPHO-USDT"
-    target_stock = "ARKM-USDT"
-    target_stock = "ORDI-USDT"
-    target_stock = "ZRO-USDT"
-    target_stock = "MEW-USDT"
+    # target_stock = "JST-USDT"
+    # target_stock = "ZRX-USDT"
+    # target_stock = "ZIL-USDT"
+    # target_stock = "BOME-USDT"
+    # target_stock = "ASTR-USDT"  # 时间短
+    # target_stock = "GOAT-USDT"  # 时间短
+    # target_stock = "ARKM-USDT"
+    # target_stock = "ORDI-USDT"
+    # target_stock = "ZRO-USDT"
+    # target_stock = "MEW-USDT"
 
     pre_day = None
     # pre_day = 1715587200000
@@ -52,8 +58,13 @@ if __name__ == '__main__':
     genius_trader = GeniusTrader()
     long_period_candle = []
 
+# 注意错误
+#         pre_day = total_candle[-1][0]
+# IndexError: list index out of range
+
     max_day = 100
-    for _ in range(max_day):
+    for i in range(max_day):
+        print(f"第: {i}次")
         # period = "1H"
         # period = "4H"
         period = "15m"
@@ -63,7 +74,7 @@ if __name__ == '__main__':
         print(pre_day)
         print()
         long_period_candle.extend(total_candle)
-        time.sleep(5)
+        time.sleep(0.1)
 
 
     long_period_candle.reverse()
