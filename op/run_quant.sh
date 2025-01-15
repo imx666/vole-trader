aa6d849ec38ec53dad79ea1ee366fe99223aaae7
imx666 <3490980474@qq.com>
2025-01-15 11:22:50 +0800

    改用supervisord启动quant

---
+++ op/run_quant.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#sh ./op/env.sh   #sh指令会创建一个新的子进程，从而使得父进程无法进入虚拟环境
+#"""source的含义在当前 shell 进程中执行指定的脚本文件，而不是启动一个新的子进程。"""
+#"""source命令可能无法找到，所以可以使用.这个符号来代替"""
+. ./op/env.sh
+
+
+#导入环境变量
+if [ ! -f .env.dev ]; then
+  echo ".env.dev 文件不存在，请创建并配置必要的环境变量。"
+  exit 1
+fi
+export $(xargs <.env.dev)
+
+
+# 启动 supervisord
+supervisord -n -c ./op/supervisord_quant.conf
+
+echo "supervisord start success!"
+
+
+
+
